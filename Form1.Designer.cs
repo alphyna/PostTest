05146e129ea1b9dfd73db1905ce9e2a13281a09f
alphyna <alphyna@xj.commufa.jp>
2019-06-22 10:35:02 +0900

    test

---
+++ Form1.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace PostTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(92, 61);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(189, 61);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(92, 90);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.Button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(189, 90);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "button4";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.Button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(92, 119);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "button5";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.Button5_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(502, 303);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Font = new System.Drawing.Font("Meiryo UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+using System.IO;
+using System.Threading;
+using System.Text.RegularExpressions;
+using Codeplex.Data;
+using TagLib;
+using TagLib.Id3v2;
+
+namespace PostTest
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        protected HttpClient httpClient = new HttpClient();
+        string joySound;
+        string petitlyric;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        private async void Button1_Click(object sender, EventArgs e)
+        {
+            string requestUri = "https://petitlyrics.com/lyrics/2730388";
+            HttpResponseMessage response;
+            string responseBody;
+            var cancellationTokenSource = new CancellationTokenSource();
+
+            response = await httpClient.GetAsync(requestUri, cancellationTokenSource.Token);
+            responseBody = await response.Content.ReadAsStringAsync();
+
+            var cookie = response.Headers.GetValues("Set-Cookie").First();
+
+            Match lm = Regex.Match(responseBody, "<script type=\"text/javascript\" src=\"(/lib/pl-lib.js.+?)\" charset=\"UTF-8\"></script>", RegexOptions.Singleline);
+
+            if (lm.Success == false)
+                return;
+
+            requestUri = "https://petitlyrics.com" + lm.Groups[1].Value;
+
+            response = await httpClient.GetAsync(requestUri, cancellationTokenSource.Token);
+            responseBody = await response.Content.ReadAsStringAsync();
+
+            lm = Regex.Match(responseBody, "jqxhr.setRequestHeader\\('X-CSRF-Token', '(.+?)'\\)", RegexOptions.Singleline);
+
+            if (lm.Success == false)
+                return;
+
+            requestUri = "https://petitlyrics.com/com/get_lyrics.ajax";
+
+            var stringContent = new StringContent("lyrics_id=2730388", Encoding.UTF8, @"application/x-www-form-urlencoded");
+
+            //stringContent.Headers.Add("Cookie", cookie);
+
+            //stringContent.Headers.Add("Host", "petitlyrics.com");
+            //stringContent.Headers.Add("Referer", "https://petitlyrics.com/lyrics/2730388");
+            stringContent.Headers.Add("X-CSRF-Token", lm.Groups[1].Value);
+            stringContent.Headers.Add("X-Requested-With", "XMLHttpRequest");
+
+            //httpClient.DefaultRequestHeaders.Host = "petitlyrics.com";
+            //httpClient.DefaultRequestHeaders.Add("Referer", "https://petitlyrics.com/lyrics/2730388");
+            //httpClient.DefaultRequestHeaders.Add("Accept", "*/*");
+            //httpClient.DefaultRequestHeaders.Add("Accept-Encoding", "gzip, deflate, br");
+            //httpClient.DefaultRequestHeaders.Add("Accept-Language", "ja,en-US;q=0.7,en;q=0.3");
+            //httpClient.DefaultRequestHeaders.Add("Cache-Control", "max-age=0");
+            //httpClient.DefaultRequestHeaders.Add("Connection", "keep-alive");
+            //httpClient.DefaultRequestHeaders.Add("Host", "petitlyrics.com");
+            //httpClient.DefaultRequestHeaders.Add("User-Agent", "Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:67.0) Gecko/20100101 Firefox/67.0");
+            //httpClient.DefaultRequestHeaders.Add("Accept", "*/*");
+
+            response = await httpClient.PostAsync(requestUri, stringContent, cancellationTokenSource.Token);
+            petitlyric = await response.Content.ReadAsStringAsync();
+
+            int i = 0;
+        }
+
+        private async void Button2_Click(object sender, EventArgs e)
+        {
+            string requestUri = "https://mspxy.joysound.com/Common/Lyric";
+            HttpResponseMessage response;
+            string responseBody;
+            var cancellationTokenSource = new CancellationTokenSource();
+
+            var stringContent = new StringContent("kind=naviGroupId&selSongNo=41743&interactionFlg=0&apiVer=1.0", Encoding.UTF8, @"application/x-www-form-urlencoded");
+
+            //httpClient.DefaultRequestHeaders.Add("Accept", "application/json, text/plain, */*");
+            //httpClient.DefaultRequestHeaders.Add("Accept-Encoding", "gzip, deflate, br");
+            //httpClient.DefaultRequestHeaders.Add("Accept-Language", "ja,en-US;q=0.7,en;q=0.3");
+            //httpClient.DefaultRequestHeaders.Add("Cache-Control", "max-age=0");
+            //httpClient.DefaultRequestHeaders.Add("Connection", "keep-alive");
+            //httpClient.DefaultRequestHeaders.Add("Host", "mspxy.joysound.com");
+            //httpClient.DefaultRequestHeaders.Add("X-JSP-APP-NAME", "0000800");
+            //httpClient.DefaultRequestHeaders.Add("Origin", "https://www.joysound.com");
+            //httpClient.DefaultRequestHeaders.Add("Referer", "https://www.joysound.com/web/search/song/41743");
+            //httpClient.DefaultRequestHeaders.Add("TE", "Trailers");
+            //httpClient.DefaultRequestHeaders.Add("User-Agent", "Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:67.0) Gecko/20100101 Firefox/67.0");
+            stringContent.Headers.Add("X-JSP-APP-NAME", "0000800");
+            stringContent.Headers.Add("Origin", "https://www.joysound.com");
+
+            response = await httpClient.PostAsync(requestUri, stringContent, cancellationTokenSource.Token);
+            joySound = await response.Content.ReadAsStringAsync();
+
+            int i = 0;
+        }
+
+        private void Button3_Click(object sender, EventArgs e)
+        {
+            var json = DynamicJson.Parse(petitlyric);
+
+            try
+            {
+                var s = json[0].lyrics;
+            }
+            catch
+            {
+
+            }
+        }
+
+        private void Button4_Click(object sender, EventArgs e)
+        {
+            var json = DynamicJson.Parse(joySound);
+
+            try
+            {
+                var s = json.lyricList[0].lyric;
+            }
+            catch
+            {
+
+            }
+        }
+
+        private void Button5_Click(object sender, EventArgs e)
+        {
+            var tf = TagLib.File.Create(@"e:\tmp\dadata.mp3");
+
+            var id3 = tf.GetTag(TagTypes.Id3v2) as TagLib.Id3v2.Tag;
+
+            ReadOnlyByteVector USLT = "USLT";
+
+            //UnsynchronisedLyricsFrame uslt;
+            //foreach (var frame in id3.GetFrames(USLT))
+            //{
+            //    uslt = frame as UnsynchronisedLyricsFrame;
+            //}
+
+            //tf.Tag.Lyrics = "ascii?";
+
+            //List<Frame> frames = new List<Frame>(id3.GetFrames(USLT));
+
+            id3.RemoveFrames(USLT);
+
+            UnsynchronisedLyricsFrame frame;
+
+            frame = UnsynchronisedLyricsFrame.Get(id3, string.Empty, "eng", true);
+
+            frame.Text = "今度こその新しい歌詞";
+
+            tf.Save();
+        }
+    }
+}
